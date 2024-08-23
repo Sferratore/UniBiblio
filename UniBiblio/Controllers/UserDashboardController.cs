db52a0de99f0a3b55732ba1bf6db5f132aeba5e8
Sferratore <sferratore.francescopaolo00@gmail.com>
2024-08-23 02:14:29 +0200

    Aggiunta pagina base per la prenotazione dei libri

--- UniBiblio/Controllers/UserDashboardController.cs
+++ UniBiblio/Controllers/UserDashboardController.cs
@@ -1,9 +1,19 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using UniBiblio.Models;
 
 namespace UniBiblio.Controllers
 {
     public class UserDashboardController : Controller
     {
+
+        private readonly unibiblioContext _context;
+
+        public UserDashboardController(unibiblioContext context)
+        {
+            _context = context;
+        }
+
         public IActionResult Index()
         {
             return View();
@@ -12,6 +22,26 @@ namespace UniBiblio.Controllers
         public IActionResult Home()
         {
             return View();
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> PrenotaLibri()
+        {
+            var libri = await _context.Libris
+                .Where(l => l.QuantitaDisponibile > 0)
+                .Select(l => new Libri
+                {
+                    IdLibro = l.IdLibro,
+                    Titolo = l.Titolo,
+                    Autore = l.Autore,
+                    AnnoPubblicazione = l.AnnoPubblicazione,
+                    Isbn = l.Isbn,
+                    Categoria = l.Categoria,
+                    QuantitaDisponibile = (int)l.QuantitaDisponibile
+                })
+                .ToListAsync();
+
+            return View(libri);
         }
     }
 }
---
+++ UniBiblio/Models/LibroViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace UniBiblio.Models
+{
+    public class LibroViewModel
+    {
+        public int Id { get; set; }
+        public string Titolo { get; set; }
+        public string Autore { get; set; }
+        public int? AnnoPubblicazione { get; set; }
+        public string ISBN { get; set; }
+        public string Categoria { get; set; }
+        public int QuantitaDisponibile { get; set; }
+        public bool IsPrenotabile => QuantitaDisponibile > 0;
+    }
+}
