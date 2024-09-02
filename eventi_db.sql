39466ad3cfde9f03e49d8e15eb29e8a00d8161ad
Sferratore <sferratore.francescopaolo00@gmail.com>
2024-09-02 19:12:56 +0200

    Aggiunto eventi di gestione record scaduti

---
+++ eventi_db.sql
@@ -0,0 +1,28 @@
+SHOW VARIABLES LIKE 'event_scheduler';
+SET GLOBAL event_scheduler = ON;
+SHOW EVENTS;
+
+DELIMITER //
+
+CREATE EVENT IF NOT EXISTS `AutoDeleteBookReservations`
+ON SCHEDULE EVERY 1 DAY STARTS CURRENT_TIMESTAMP
+DO 
+BEGIN
+    DELETE FROM Prenotazioni_Libri
+    WHERE DATEDIFF(CURRENT_DATE, data_prenotazione) > 7 AND data_ritiro IS NULL;
+END;
+
+DELIMITER ;
+
+
+DELIMITER //
+
+CREATE EVENT IF NOT EXISTS `AutoDeleteRoomReservations`
+ON SCHEDULE EVERY 1 DAY STARTS CURRENT_TIMESTAMP
+DO 
+BEGIN
+    DELETE FROM Prenotazioni_Sale
+    WHERE giorno_prenotato < CURRENT_DATE;
+END;
+
+DELIMITER ;
